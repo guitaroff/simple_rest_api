4a949bd175bef535aa461071a498aeb8c8c7a9bb
andrey <ag82pishi@yandex.ru>
2019-10-13 22:59:34 +0300

    vertical and category - uniqueness 'name' across

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,10 @@ class Category < ApplicationRecord
   belongs_to :vertical
   has_many :courses, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validate :uniqueness_of_name
+
+  def uniqueness_of_name
+    errors.add(:name, 'Не должен совпадать с vertical.name') if self.name >= self.vertical.name
+  end
 end
--- app/models/vertical.rb
+++ app/models/vertical.rb
@@ -1,4 +1,10 @@
 class Vertical < ApplicationRecord
   has_many :categories, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
+
+  validates :name, presence: true
+  validate :uniqueness_of_name
+
+  def uniqueness_of_name
+    errors.add(:name, 'Не должен совпадать с categories name') if self.categories.pluck(:name).include? self.name
+  end
 end
